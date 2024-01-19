89e666c4b2a7f13bf12605920bd98885de95560c
David Ruiz Luque <daavidruiz01@outlook.com>
2024-01-19 23:15:12 +0100

    new branch

--- src/commsManager.c
+++ src/commsManager.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-#include "../lib/mosquitto.h"
+#include <mosquitto.h>
+
+void on_message(struct mosquitto *mosca, void *userdata, const struct mosquitto_message *message){
+    // Manejar el mensaje recibido aquí
+    printf("Mensaje recibido en el topic %s: %s\n", message->topic, (char *)message->payload);
+}
 
 int main(void){
     int rc;
     struct mosquitto *mosca;
-    mosquitto_lib_init();    
 
-    mosca = mosquitto_new("mqtt1",true,NULL);
+    mosquitto_lib_init();
 
-    rc = mosquitto_connect(mosca,"127.0.0.1",1883,60);
+    mosca = mosquitto_new("mqtt_server", true, NULL);
 
-    if(rc != 0){
-        printf("Client could not connect to broker!. Error: %s\n",mosquitto_strerror(rc));
-        mosquitto_destroy(mosca);
+    // Configurar el callback para manejar los mensajes recibidos
+    mosquitto_message_callback_set(mosca, on_message);
+
+    // Iniciar el servidor MQTT en el puerto 1883
+    rc = mosquitto_connect(mosca, "127.0.0.1", 1883, 1024);
+
+    if(rc){
+        printf("Error al iniciar el servidor MQTT. Código de retorno: %d\n", rc);
         return -1;
     }
-    printf("Connected to the broker!\n");
 
-    mosquitto_publish(mosca,NULL,"/",20,"RPI mosca rafa feo",0,true);
+    printf("Servidor MQTT iniciado en el puerto 1883...\n");
 
-    while(1){
+    // Entrar en el bucle de red para manejar eventos
+    mosquitto_loop_forever(mosca, -1, 1);
 
-    }
-    mosquitto_disconnect(mosca);
+    // Cleanup
     mosquitto_destroy(mosca);
     mosquitto_lib_cleanup();
+
     return 0;
-    
 }
